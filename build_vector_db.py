7e606f780a18ee5b99b8e8ea266a35a83ee05e12
vaibhavkumar39 <vaibhavkumar39.00@gmail.com>
2025-07-12 23:02:01 +0530

    Update build_vector_db.py

--- build_vector_db.py
+++ build_vector_db.py
@@ -8,22 +8,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def build_vector_db(pdf_path: str, db_path: str = "vector_db"):
-    print("🔍 Loading PDF:", pdf_path)
+    print("--------------------Loading PDF:", pdf_path)
     loader = PyPDFLoader(pdf_path)
     docs = loader.load()
 
-    print("✂️ Splitting text...")
+    print("---------------------Splitting text...")
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     chunks = splitter.split_documents(docs)
 
-    print("🧠 Generating embeddings...")
+    print("----------------------Generating embeddings...")
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-    print("💾 Building and saving vector store...")
+    print("-----------------------Building and saving vector store...")
     vectorstore = FAISS.from_documents(chunks, embedding=embeddings)
     vectorstore.save_local(folder_path=db_path)
 
-    print(f"✅ Vector DB saved to: {db_path}")
+    print(f"----------------------Vector DB saved to: {db_path}")
 
 if __name__ == "__main__":
 
